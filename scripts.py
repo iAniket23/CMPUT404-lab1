6f66dddc910c560e839633d3769d8169b84cc7dc
Aniket Mishra <ianiket23@gmail.com>
2021-09-10 04:40:41 -0600

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -3,3 +3,5 @@ print(requests.__version__)
 
 website_Status = requests.get("http://google.com")
 print(website_Status.status_code)
+
+https://raw.githubusercontent.com/iAniket23/CMPUT404-lab1/main/scripts.py
